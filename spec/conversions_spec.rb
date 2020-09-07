c3077ca59e5e426bc61b64c5e10f4f11f6830e50
Allison Ricker <alliricker@gmail.com>
2020-09-07 16:43:00 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -1,10 +1,9 @@
+require_relative '../conversions.rb'
 describe "conversions" do
   desribe "ounces_to_grams" do
     it "given 0, returns 0.0" do
       grams = ounces_to_grams(0)
       expect(grams).to  eq(0.0)
     end
-
   end
-
 end
